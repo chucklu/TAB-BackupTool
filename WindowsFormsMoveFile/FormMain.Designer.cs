279d89c46cfc5753c5a4a087ad68f8cfca8a3a15
Chuck LU <chucklu2015@outlook.com>
2022-08-21 15:27:17 +0800

    initial version

---
+++ WindowsFormsMoveFile/FormMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace WindowsFormsMoveFile
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMoveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonMoveFile
+            // 
+            this.buttonMoveFile.Location = new System.Drawing.Point(465, 164);
+            this.buttonMoveFile.Name = "buttonMoveFile";
+            this.buttonMoveFile.Size = new System.Drawing.Size(111, 53);
+            this.buttonMoveFile.TabIndex = 0;
+            this.buttonMoveFile.Text = "MoveFile";
+            this.buttonMoveFile.UseVisualStyleBackColor = true;
+            this.buttonMoveFile.Click += new System.EventHandler(this.buttonMoveFile_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonMoveFile);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMoveFile;
+    }
+}
+
---
+++ WindowsFormsMoveFile/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsMoveFile
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMoveFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sourceFolder = GetSourceFolder();
+
+                var backupRootFolder = Path.Combine(sourceFolder, "Backups");
+                CreateFolder(backupRootFolder);
+
+                var monthFolder = Path.Combine(backupRootFolder, DateTime.Now.ToString("yyyyMM"));
+                CreateFolder(monthFolder);
+
+                var backupFolder = Path.Combine(monthFolder, DateTime.Now.ToString("yyyy-MM-dd HHmmss"));
+                CreateFolder(backupFolder);
+
+                var files = Directory.GetFiles(sourceFolder);
+                foreach (var file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    var targetFileName = Path.Combine(backupFolder, fileName);
+                    File.Copy(file, targetFileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private string GetSourceFolder()
+        {
+            var sourceFolder = ConfigurationManager.AppSettings["SourceFolder"];
+            if (string.IsNullOrWhiteSpace(sourceFolder))
+            {
+                throw new Exception("Can not find source folder in app setting.");
+            }
+
+            if (!Directory.Exists(sourceFolder))
+            {
+                throw new Exception("Source folder not exist");
+            }
+
+            return sourceFolder;
+        }
+
+        private void CreateFolder(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+    }
+}
